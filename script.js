706ed21ba9a99145b15d0c0453b2b305ec450d2d
Nick Griffen <nicholas.s.griffen@gmail.com>
2018-08-30 16:08:42 -0600

    working board, color picker, cell coloring

---
+++ script.js
@@ -0,0 +1,44 @@
+const board = document.getElementById('board')
+const cellSize = getCssVar("--cell-size")
+const numColumns = getCssVar("--number-of-columns")
+const numRows = getCssVar("--number-of-rows")
+const pen = document.getElementById('pen')
+const picker = document.getElementById('color')
+
+buildBoard(numColumns, numRows)
+setEventListeners()
+
+function buildBoard(columns, rows) {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      let cell = document.createElement('div')
+      cell.style += `grid-column-start: ${i}; grid-row-start: ${j}`
+      cell.classList.add('cell')
+      board.appendChild(cell)
+    }
+  }
+}
+
+function setEventListeners() {
+  board.addEventListener("click", colorCell)
+  picker.addEventListener("change", changeColor)
+}
+
+function changeColor(e) {
+  setCssVar("--picked-color", this.value)
+}
+
+function colorCell(e) {
+  let color = getCssVar("--picked-color")
+  if (e.target.classList.contains('cell')) {
+    e.target.style.setProperty("background-color", color)
+  }
+}
+
+function getCssVar(prop) {
+  return getComputedStyle(board).getPropertyValue(prop)
+}
+
+function setCssVar(prop, value) {
+  board.style.setProperty(prop, value)
+}
